4a387a101dac8f672dd9129839659ec80db7ab39
Mohamed Yahya Kharrat <kharratyahyakhy@gmail.com>
2026-03-30 10:17:16 +0100

    verision with the role of the admin

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
 
 const app = express();
+
 const corsOptions = {
     origin: FRONTEND_URL,
     credentials: true,
